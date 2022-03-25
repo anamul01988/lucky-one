a72a969fe0715bcfbc07216eaf5f8a1a4916cd47
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-03-26 03:07:06 +0600

    header portion has done

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+      <div className="container header">
+        <div className="row">
+            <div className="col-lg-12 col-sm-12">
+                <div className="content py-3">
+                    <h4 className="middle">Global Educational Site</h4>
+                    <h1>Total Number Of Student! 20,000</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default Header;
